6d00f076f41f7b933693a28aee5363fd4e0d386c
Tin Nguyen <78500429+tintinator1@users.noreply.github.com>
2025-10-19 15:45:43 -0700

    handle userNotFoundError to be 401 instead of 500

--- backend/auth/service/routers/auth.py
+++ backend/auth/service/routers/auth.py
@@ -9,6 +9,7 @@ from service.exceptions.user import (
     UserEmailExistsError,
     UserInvalidCreationInputError,
     UserUsernameExistsError,
+    UserNotFoundError
 )
 from service.models.user import User, UserCreate
 from service.security.token import (
@@ -65,7 +66,7 @@ async def login(
         user = await auth_service.authenticate_user_by_username(
             form_data.username, form_data.password
         )
-    except AuthIncorrectPasswordError:
+    except (UserNotFoundError,AuthIncorrectPasswordError):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect credentials"
         )
