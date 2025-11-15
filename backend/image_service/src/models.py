ccb98e4e9cf42eb48bf5692b94a0ea5657e1764c
Nick Fan <nicolasfan51@gmail.com>
2025-11-14 16:03:20 -0800

    more cleanup

--- backend/image_service/src/models.py
+++ backend/image_service/src/models.py
@@ -14,36 +14,3 @@ class Blob(BaseModel):
 
 class Image(Blob):
     uploader_user_id: str
-
-
-"""
-User Flow Notes:
-
-Create profile: id = 0
-
-get_image(id=0)
-- query image in DB with ID
-- return url
-
-upload_pfp_image(img_bytes)
-- upload_image(id, new?)
-    - create uuid
-    - azure upload image
-    - DB save image info with ID, URL
-    - return image ID
-- user profile update pfp ID
-
-
-delete_img(id=x)
-- query image in DB with ID
-- delete image in blob
-
-DB
-- create new image metadata
-- delete image metadata
-- find image metadata (based on ID)
-
-Blob
-- upload, return url
-- delete given url
-"""
