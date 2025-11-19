5da0ad2c74f0904f57f35fb926769d38ae26bd2a
Nick Fan <nicolasfan51@gmail.com>
2025-11-19 01:20:08 -0800

    users pytest

--- backend/users_service/tests/test_router.py
+++ backend/users_service/tests/test_router.py
@@ -6,10 +6,10 @@ from shared.exceptions.db import (
     RecordNotFoundError,
     RecordUpdateError,
 )
-
 from src.models import UserUpdate
 
 
+# --- Tests for /me ---
 class TestRouterMe:
     @pytest.mark.asyncio
     async def test_get_me_success(self, client, mock_service, sample_user_in_db):
@@ -21,14 +21,13 @@ class TestRouterMe:
         assert data["id"] == sample_user_in_db.id
 
 
+# --- Tests for /id/{user_id} ---
 class TestRouterGetUserByID:
     @pytest.mark.asyncio
-    async def test_get_user_by_id_success(
-        self, client, mock_service, sample_user_in_db
-    ):
+    async def test_get_user_by_id_success(self, client, mock_service, sample_user_in_db):
         mock_service.get_user_by_id.return_value = sample_user_in_db
 
-        response = client.get("/user123")
+        response = client.get("/id/user123")
         assert response.status_code == 200
         data = response.json()
         assert data["id"] == sample_user_in_db.id
@@ -37,29 +36,28 @@ class TestRouterGetUserByID:
     async def test_get_user_by_id_not_found(self, client, mock_service):
         mock_service.get_user_by_id.side_effect = RecordNotFoundError()
 
-        response = client.get("/user123")
+        response = client.get("/id/user123")
         assert response.status_code == 404
 
     @pytest.mark.asyncio
     async def test_get_user_by_id_query_error(self, client, mock_service):
         mock_service.get_user_by_id.side_effect = GeneralQueryError()
 
-        response = client.get("/user123")
+        response = client.get("/id/user123")
         assert response.status_code == 500
 
     @pytest.mark.asyncio
     async def test_get_user_by_id_unexpected_error(self, client, mock_service):
         mock_service.get_user_by_id.side_effect = Exception()
 
-        response = client.get("/user123")
+        response = client.get("/id/user123")
         assert response.status_code == 500
 
 
+# --- Tests for PATCH / ---
 class TestRouterUserUpdate:
     @pytest.mark.asyncio
-    async def test_update_user_success_empty(
-        self, client, mock_service, sample_user_in_db
-    ):
+    async def test_update_user_success_empty(self, client, mock_service, sample_user_in_db):
         mock_service.update_user.return_value = sample_user_in_db
         user_update = UserUpdate(id="user123")
 
@@ -69,7 +67,6 @@ class TestRouterUserUpdate:
     @pytest.mark.asyncio
     async def test_update_user_not_found(self, client, mock_service):
         mock_service.update_user.side_effect = RecordNotFoundError()
-
         user_update = UserUpdate(id="user123")
 
         response = client.patch("/", json=user_update.model_dump())
@@ -78,7 +75,6 @@ class TestRouterUserUpdate:
     @pytest.mark.asyncio
     async def test_update_user_not_authorized(self, client, mock_service):
         mock_service.update_user.side_effect = AuthError()
-
         user_update = UserUpdate(id="user321")
 
         response = client.patch("/", json=user_update.model_dump())
@@ -87,7 +83,6 @@ class TestRouterUserUpdate:
     @pytest.mark.asyncio
     async def test_update_user_update_error(self, client, mock_service):
         mock_service.update_user.side_effect = RecordUpdateError()
-
         user_update = UserUpdate(id="user321")
 
         response = client.patch("/", json=user_update.model_dump())
@@ -96,7 +91,6 @@ class TestRouterUserUpdate:
     @pytest.mark.asyncio
     async def test_update_user_query_error(self, client, mock_service):
         mock_service.update_user.side_effect = GeneralQueryError()
-
         user_update = UserUpdate(id="user321")
 
         response = client.patch("/", json=user_update.model_dump())
@@ -105,16 +99,13 @@ class TestRouterUserUpdate:
     @pytest.mark.asyncio
     async def test_update_user_unexpected_error(self, client, mock_service):
         mock_service.update_user.side_effect = Exception()
-
         user_update = UserUpdate(id="user321")
 
         response = client.patch("/", json=user_update.model_dump())
         assert response.status_code == 500
 
     @pytest.mark.asyncio
-    async def test_update_user_success_username(
-        self, client, mock_service, sample_user_in_db
-    ):
+    async def test_update_user_success_username(self, client, mock_service, sample_user_in_db):
         new_sample_user_in_db = sample_user_in_db.model_copy(deep=True)
         new_sample_user_in_db.username = "newtestuser"
         mock_service.update_user.return_value = new_sample_user_in_db
@@ -127,9 +118,7 @@ class TestRouterUserUpdate:
         assert data["username"] == "newtestuser"
 
     @pytest.mark.asyncio
-    async def test_update_user_success_email(
-        self, client, mock_service, sample_user_in_db
-    ):
+    async def test_update_user_success_email(self, client, mock_service, sample_user_in_db):
         new_sample_user_in_db = sample_user_in_db.model_copy(deep=True)
         new_sample_user_in_db.email = "newtest@gmail.com"
         mock_service.update_user.return_value = new_sample_user_in_db
@@ -142,45 +131,33 @@ class TestRouterUserUpdate:
         assert data["email"] == "newtest@gmail.com"
 
 
+# --- Tests for DELETE /{user_id} ---
 class TestRouterUserDelete:
     @pytest.mark.asyncio
     async def test_delete_user_success(self, client, mock_service):
         response = client.delete("/user123")
-
         assert response.status_code == 204
 
     @pytest.mark.asyncio
-    async def test_delete_user_not_authorized(
-        self,
-        client,
-        mock_service,
-    ):
+    async def test_delete_user_not_authorized(self, client, mock_service):
         mock_service.delete_user.side_effect = AuthError()
-
         response = client.delete("/user123")
-
         assert response.status_code == 403
 
     @pytest.mark.asyncio
     async def test_delete_user_not_found(self, client, mock_service):
         mock_service.delete_user.side_effect = RecordNotFoundError()
-
         response = client.delete("/user123")
-
         assert response.status_code == 404
 
     @pytest.mark.asyncio
     async def test_delete_user_delete_error(self, client, mock_service):
         mock_service.delete_user.side_effect = RecordDeletionError()
-
         response = client.delete("/user123")
-
         assert response.status_code == 500
 
     @pytest.mark.asyncio
     async def test_delete_user_unexpected_error(self, client, mock_service):
         mock_service.delete_user.side_effect = Exception()
-
         response = client.delete("/user123")
-        print(response.content)
         assert response.status_code == 500
